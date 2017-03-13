bed08c7f87e4a633de285e377812af6bac5485a6
Tom Bailey <tombailey1@hotmail.co.uk>
2017-03-13 22:13:35 +0000

    Fix syntax

--- android/src/main/java/com/msopentech/thali/toronionproxy/exception/TorNotRunningException.java
+++ android/src/main/java/com/msopentech/thali/toronionproxy/exception/TorNotRunningException.java
@@ -1,6 +1,6 @@
 package com.msopentech.thali.toronionproxy.exception;
 
-public class TorNotRunningException extends Exception() {
+public class TorNotRunningException extends Exception {
 
     public TorNotRunningException() {
 
